b5cbbae5c03cc5c93a334430909aecccbb77bf54
Lucas Xia <lucasxia01@gmail.com>
2024-05-04 18:41:40 -0400

    setting up poly commit and zerotest

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,30 @@ mod prover;
 
 #[cfg(test)]
 mod tests {
+    use ark_poly_commit::PolynomialCommitment;
+
     use super::*;
     #[test]
     fn prove_and_verify() {
         // create a prover from prover.rs
         use ark_poly_commit::marlin_pc::MarlinKZG10;
+        type PC = MarlinKZG10<Bls12_381, DensePolynomial<Fr>>;
         type FS = SimpleHashFiatShamirRng<Blake2s, ChaChaRng>;
+        let max_degree = 1024;
+        let rng = &mut ark_std::test_rng();
+        let srs = PC::setup(max_degree, None, rng).unwrap();
+        let (committer_key, verifier_key) = PC::trim(
+            &srs,
+            max_degree,
+            0,
+            None,
+        ).unwrap();
+
         // just assume the coset domain is 1 for now
-        let f_evals = vec![Fr::from(1), Fr::from(2), Fr::from(1), Fr::from(2)];
+        let f_evals = vec![Fr::from(1), Fr::from(2), Fr::from(1), Fr::from(2), Fr::from(1), Fr::from(1), Fr::from(2), Fr::from(1)];
         let t_evals = vec![Fr::from(1), Fr::from(2)];
-        let prover = Prover::<Fr, MarlinKZG10<Bls12_381, DensePolynomial<Fr>>, FS> ::new(f_evals, t_evals, 1);
+        
+        let prover = Prover::<Fr, PC, FS>::new(committer_key, f_evals, t_evals, 1);
+        prover.prove();
     }
 }
--- src/prover.rs
+++ src/prover.rs
@@ -2,11 +2,10 @@
 
 use std::{collections::HashMap, hash::Hash, marker::PhantomData};
 
-use ark_ff::{to_bytes, FftField, PrimeField, UniformRand};
-use ark_poly::{Evaluations, 
-    univariate::DensePolynomial, EvaluationDomain, GeneralEvaluationDomain, Radix2EvaluationDomain
+use ark_ff::{to_bytes, FftField, UniformRand};
+use ark_poly::{univariate::SparsePolynomial, univariate::DensePolynomial, EvaluationDomain, Evaluations, GeneralEvaluationDomain, Radix2EvaluationDomain
 };
-use ark_poly_commit::{PolynomialCommitment};
+use ark_poly_commit::{LabeledPolynomial, PolynomialCommitment};
 
 use std::fmt::Error;
 
@@ -27,7 +26,12 @@ pub fn poly_from_evals<F: FftField>(evals: &Vec<F>) -> DensePolynomial<F> {
     eval_form.interpolate()
 }
 
-// pub struct Proof<F: PrimeField> {
+pub fn get_mult_subgroup_vanishing_poly<F: FftField>(n: usize) -> SparsePolynomial<F> {
+    let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
+    domain.vanishing_polynomial()
+}
+
+// pub struct Proof<F: FftField> {
 
 // }
 
@@ -35,6 +39,7 @@ pub struct Prover<F: FftField, PC: PolynomialCommitment<F, DensePolynomial<F>>, 
     // f_domain: GeneralEvaluationDomain<F>,
     // t_domain: GeneralEvaluationDomain<F>,
     // coset_domain: GeneralEvaluationDomain<F>,
+    commitment_key : PC::CommitterKey,
     f_evals : Vec<F>,
     t_evals : Vec<F>,
     f : DensePolynomial<F>,
@@ -44,19 +49,20 @@ pub struct Prover<F: FftField, PC: PolynomialCommitment<F, DensePolynomial<F>>, 
     coset_domain_size : usize
 }
 
-impl<F: PrimeField, PC: PolynomialCommitment<F, DensePolynomial<F>>, FS: FiatShamirRng>
+impl<F: FftField, PC: PolynomialCommitment<F, DensePolynomial<F>>, FS: FiatShamirRng>
     Prover<F, PC, FS>
 {
 
     pub const PROTOCOL_NAME: &'static [u8] = b"MUX_Proofs";
 
-    pub fn new(f_evals : Vec<F>, t_evals : Vec<F>, coset_domain_size : usize) -> Self {
+    pub fn new(commitment_key : PC::CommitterKey, f_evals : Vec<F>, t_evals : Vec<F>, coset_domain_size : usize) -> Self {
         // assert_eq!(f_domain.size(), f_evals.len());
         // assert_eq!(t_domain.size(), t_evals.len());
         // self.f_domain = f_domain;
         // self.t_domain = t_domain;
         // self.coset_domain = coset_domain;
         return Self {
+            commitment_key,
             f_evals : f_evals.clone(),
             t_evals : t_evals.clone(),
             f : poly_from_evals(&f_evals),
@@ -73,6 +79,7 @@ impl<F: PrimeField, PC: PolynomialCommitment<F, DensePolynomial<F>>, FS: FiatSha
     }
 
     pub fn prove(self) -> Result<Vec<u8>, Error> {
+        
         // TODO: fix this initialization to include all public inputs
         let mut fs_rng = FS::initialize(
             &to_bytes![&Self::PROTOCOL_NAME].unwrap(),
@@ -83,6 +90,9 @@ impl<F: PrimeField, PC: PolynomialCommitment<F, DensePolynomial<F>>, FS: FiatSha
         assert_eq!(t_domain_size % self.coset_domain_size, 0);
         let f_domain_num_cosets = f_domain_size / self.coset_domain_size;
         let t_domain_num_cosets = t_domain_size / self.coset_domain_size;
+        // Set up the vanishing polynomials for the two domains
+        let t_vanishing: DensePolynomial<F> = get_mult_subgroup_vanishing_poly(t_domain_size).into();
+        let f_vanishing: DensePolynomial<F> = get_mult_subgroup_vanishing_poly(f_domain_size).into();
         // Step 1: compute count polynomial c(X) that encodes the counts the frequency of each table vector in f
         let f_vecs: Vec<Vec<F>> = (0..f_domain_num_cosets)
             .map(|coset_idx| {
@@ -114,16 +124,26 @@ impl<F: PrimeField, PC: PolynomialCommitment<F, DensePolynomial<F>>, FS: FiatSha
             }
         }
         // Step 1.a: Define c(X) over t_domain
-        let mut c_evals: Vec<i32> = vec![0; t_domain_size];
+        let mut c_evals: Vec<F> = vec![F::zero(); t_domain_size];
         for i  in 0..t_domain_size {
-            c_evals[i] = f_vec_counts[&t_vecs[i%t_domain_num_cosets].clone()];
+            // c_evals[i] = f_vec_counts[&t_vecs[i%t_domain_num_cosets].clone()];
         }
-        // Step 1.b: ZeroTest to should that c(\gammaX) = c(X)
-        
-        
-        println!("{:?}", c_evals);
-        
+        println!("Count poly in eval form: {:?}", c_evals);
 
+        // Step 1.b: ZeroTest to should that c(\gammaX) = c(X)
+        // commit to c(X) and c(\gammaX)
+        let c_coeffs = poly_from_evals(&c_evals);
+        // rotate c_evals by 1
+        c_evals.rotate_right(1);
+        println!("Rotated c_evals: {:?}", c_evals);
+        let c_coeffs_rotated = poly_from_evals(&c_evals);
+        // Want to prove that c(X) - c(\gammaX) = 0
+        let c_com = LabeledPolynomial::new("c".to_string(), c_coeffs, None, None);
+        let c_rotated_com = LabeledPolynomial::new("c_rotated".to_string(), c_coeffs_rotated, None, None);
+        let (c_comms, _) = PC::commit(&self.commitment_key, vec![&c, &c_rotated], None).unwrap();
+        // Now that we've committed, do a zero test over t_domain.
+        // Get a quotient poly q(X) = (c(X) - c(\gammaX))/t_vanishing over t_domain
+        let c_quotient = (c_coeffs.sub(&c_coeffs_rotated)).div(&t_vanishing);
 
         // Step 2: Compute challenges alpha and beta
         let alpha: F = u128::rand(&mut fs_rng).into();
