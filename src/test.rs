3e2e29c73d081c0a5b818f919c837490ae9379b1
Lucas Xia <lucasxia01@gmail.com>
2024-05-25 03:03:41 -0400

    added medium test (currently fails)

--- src/test.rs
+++ src/test.rs
@@ -139,4 +139,73 @@ mod tests {
 
         assert!(result);
     }
+
+    // Test with subvector size of 4, f size of 8, t size of 4
+    #[test]
+    fn prove_and_verify_medium() {
+        // create a prover from prover.rs
+        use ark_poly_commit::marlin_pc::MarlinKZG10;
+        type PC = MarlinKZG10<Bls12_381, DensePolynomial<Fr>>;
+        type FS = SimpleHashFiatShamirRng<Blake2s, ChaChaRng>;
+        type CosetLookupInst = CosetLookup<Fr, PC, FS>;
+        let max_degree = 1024;
+        let rng = &mut ark_std::test_rng();
+
+        // just assume the coset domain is 1 for now
+        let f_evals = vec![
+            Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(2),
+            Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(3),
+            Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(2),
+            Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(2), Fr::from(1), Fr::from(1), Fr::from(1), Fr::from(3)
+        ];
+        
+        let t_evals = vec![
+            Fr::from(1), Fr::from(1), Fr::from(2), Fr::from(1),
+            Fr::from(2), Fr::from(1), Fr::from(1), Fr::from(3),
+            Fr::from(1), Fr::from(1), Fr::from(2), Fr::from(1),
+            Fr::from(2), Fr::from(1), Fr::from(1), Fr::from(3),
+        ]; // (1, 2, 1, 2), (1, 1, 1, 1), (2, 1, 2, 1), (2, 3, 2, 3)
+        let subvector_size = 2;
+        // Universal setup
+        let universal_srs = CosetLookupInst::universal_setup(64, rng).unwrap();
+
+        // Index to generate the prover and verifier keys
+        let (committer_key, verifier_key) = CosetLookupInst::index(&universal_srs, 64).unwrap();
+
+        let f = LabeledPolynomial::new("f".to_string(), poly_from_evals(&f_evals), None, None);
+        let t = LabeledPolynomial::new("t".to_string(), poly_from_evals(&t_evals), None, None);
+        // Get the public statement: the commitments to f and t
+        let f_comm = PC::commit(&committer_key, vec![&f], None).unwrap().0[0].clone();
+        let t_comm = PC::commit(&committer_key, vec![&t], None).unwrap().0[0].clone();
+
+        let f_domain = Radix2EvaluationDomain::<Fr>::new(f_evals.len()).unwrap();
+        let t_domain = Radix2EvaluationDomain::<Fr>::new(t_evals.len()).unwrap();
+        let coset_domain = Radix2EvaluationDomain::<Fr>::new(subvector_size).unwrap();
+        // Prove
+        let proof = CosetLookupInst::prove(
+            &committer_key,
+            &f_domain,
+            &t_domain,
+            &coset_domain,
+            &f_comm,
+            &t_comm,
+            f_evals,
+            t_evals,
+            f,
+            t,
+        )
+        .unwrap();
+        let result = CosetLookupInst::verify(
+            &verifier_key,
+            &f_domain,
+            &t_domain,
+            &coset_domain,
+            &proof,
+            &f_comm,
+            &t_comm,
+        )
+        .unwrap();
+
+        assert!(result);
+    }
 }
