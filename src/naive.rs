cc19eb39aa169dd14b5bf23b982a2b828e09336a
MercysJest <wdnguyen@stanford.edu>
2024-05-24 21:02:19 -0700

    adds prover round functions

--- src/naive.rs
+++ src/naive.rs
@@ -35,31 +35,30 @@ pub fn compute_terms(numerator: Option<&[Fr]>, denominator: &[Fr], alpha: Fr) ->
     }
 }
 
-pub fn compute_starter_polys(
+pub fn compute_round_2_polys(
     f_vec: &[Fr],
     t_vec: &[Fr],
     c_vec: &[Fr],
+    V: Radix2EvaluationDomain<Fr>,
+    H: Radix2EvaluationDomain<Fr>,
 ) -> (
     DensePolynomial<Fr>,
     DensePolynomial<Fr>,
     DensePolynomial<Fr>,
 ) {
-    let n = f_vec.len();
-    let d = t_vec.len();
-    let V = Radix2EvaluationDomain::<Fr>::new(n).unwrap();
-    let H = Radix2EvaluationDomain::<Fr>::new(d).unwrap();
-
     let f = Evaluations::from_vec_and_domain(f_vec.to_vec(), V).interpolate();
     let t = Evaluations::from_vec_and_domain(t_vec.to_vec(), H).interpolate();
     let c = Evaluations::from_vec_and_domain(c_vec.to_vec(), H).interpolate();
     (f, t, c)
 }
 
-pub fn compute_helper_polys(
+pub fn compute_round_3_polys(
     f_vec: &[Fr],
     t_vec: &[Fr],
     c_vec: &[Fr],
     alpha: Fr,
+    V: Radix2EvaluationDomain<Fr>,
+    H: Radix2EvaluationDomain<Fr>,
 ) -> (
     DensePolynomial<Fr>,
     DensePolynomial<Fr>,
@@ -69,18 +68,13 @@ pub fn compute_helper_polys(
     DensePolynomial<Fr>,
     Fr,
 ) {
-    let n = f_vec.len();
-    let d = t_vec.len();
-    let V = Radix2EvaluationDomain::<Fr>::new(n).unwrap();
-    let H = Radix2EvaluationDomain::<Fr>::new(d).unwrap();
-
     let f_terms = compute_terms(None, &f_vec, alpha);
     let t_terms = compute_terms(Some(&c_vec), &t_vec, alpha);
 
     let sum: Fr = f_terms.iter().sum();
 
-    let f_shift = sum / Fr::from(n as u64);
-    let t_shift = sum / Fr::from(d as u64);
+    let f_shift = sum / Fr::from(V.size() as u64);
+    let t_shift = sum / Fr::from(H.size() as u64);
     let shifted_f_terms = f_terms.iter().map(|x| *x - f_shift).collect::<Vec<Fr>>();
     let shifted_t_terms = t_terms.iter().map(|x| *x - t_shift).collect::<Vec<Fr>>();
     let mut s_f_vec = consecutive_sum(&shifted_f_terms);
@@ -97,8 +91,40 @@ pub fn compute_helper_polys(
     (f_hab, t_hab, s_f, s_t, s_f_rot, s_t_rot, sum)
 }
 
-pub fn blah() {
-    let f_vec = vec![
+pub fn compute_round_4_polys(
+    f: &DensePolynomial<Fr>,
+    t: &DensePolynomial<Fr>,
+    c: &DensePolynomial<Fr>,
+    f_hab: &DensePolynomial<Fr>,
+    t_hab: &DensePolynomial<Fr>,
+    s_f: &DensePolynomial<Fr>,
+    s_t: &DensePolynomial<Fr>,
+    s_f_rot: &DensePolynomial<Fr>,
+    s_t_rot: &DensePolynomial<Fr>,
+    sum: Fr,
+    alpha: Fr,
+    zeta: Fr,
+    V: Radix2EvaluationDomain<Fr>,
+    H: Radix2EvaluationDomain<Fr>,
+) -> (DensePolynomial<Fr>, DensePolynomial<Fr>) {
+    let one_poly = DensePolynomial::from_coefficients_vec(vec![Fr::one()]);
+    let alpha_poly = DensePolynomial::from_coefficients_vec(vec![alpha]);
+    let s_n_poly = DensePolynomial::from_coefficients_vec(vec![sum / Fr::from(V.size() as u64)]);
+    let s_d_poly = DensePolynomial::from_coefficients_vec(vec![sum / Fr::from(H.size() as u64)]);
+    let zt_V_1 = &(&(f + &alpha_poly) * f_hab) - &one_poly;
+    let zt_V_2 = &(s_f + f_hab) - &(s_f_rot - &s_n_poly);
+    let zt_V = &zt_V_1 * (&zt_V_2.mul(zeta));
+    let zt_H_1 = &(&(t + &alpha_poly) * t_hab) - c;
+    let zt_H_2 = &(s_t + t_hab) - &(s_t_rot - &s_d_poly);
+    let zt_H = &zt_H_1 * (&zt_H_2.mul(zeta));
+
+    let q_V = zt_V.divide_by_vanishing_poly(V).unwrap().0;
+    let q_H = zt_V.divide_by_vanishing_poly(H).unwrap().0;
+    (q_V, q_H)
+}
+
+pub fn prover(f_vec: &[Fr], t_vec: &[Fr], c_vec: &[Fr]) {
+    /*let f_vec = vec![
         Fr::from(1),
         Fr::from(1),
         Fr::from(1),
@@ -109,30 +135,38 @@ pub fn blah() {
         Fr::from(2),
     ];
     let t_vec = vec![Fr::from(1), Fr::from(2)];
-    let c_vec = vec![Fr::from(5), Fr::from(3)];
+    let c_vec = vec![Fr::from(5), Fr::from(3)];*/
 
     let n = f_vec.len();
     let d = t_vec.len();
     let V = Radix2EvaluationDomain::<Fr>::new(n).unwrap();
     let H = Radix2EvaluationDomain::<Fr>::new(d).unwrap();
 
+    // Round 2
+    let (f, t, c) = compute_round_2_polys(&f_vec, &t_vec, &c_vec, V.clone(), H.clone());
     let alpha = Fr::from(3);
-    let zeta = Fr::from(5);
-    let one_poly = DensePolynomial::from_coefficients_vec(vec![Fr::one()]);
-    let alpha_poly = DensePolynomial::from_coefficients_vec(vec![alpha]);
-    let (f, t, c) = compute_starter_polys(&f_vec, &t_vec, &c_vec);
+
+    // Round 3
     let (f_hab, t_hab, s_f, s_t, s_f_rot, s_t_rot, sum) =
-        compute_helper_polys(&f_vec, &t_vec, &c_vec, alpha);
+        compute_round_3_polys(&f_vec, &t_vec, &c_vec, alpha, V.clone(), H.clone());
+    let zeta = Fr::from(5);
 
-    let s_n_poly = DensePolynomial::from_coefficients_vec(vec![sum / Fr::from(n as u64)]);
-    let s_d_poly = DensePolynomial::from_coefficients_vec(vec![sum / Fr::from(d as u64)]);
-    let zt_V_1 = &(&(&f + &alpha_poly) * &f_hab) - &one_poly;
-    let zt_V_2 = &(&s_f + &f_hab) - &(&s_f_rot - &s_n_poly);
-    let zt_V = &zt_V_1 * (&zt_V_2.mul(zeta));
-    let zt_H_1 = &(&(&t + &alpha_poly) * &t_hab) - &c;
-    let zt_H_2 = &(&s_t + &t_hab) - &(&s_t_rot - &s_d_poly);
-    let zt_H = &zt_H_1 * (&zt_H_2.mul(zeta));
-
-    let q_V = zt_V.divide_by_vanishing_poly(V).unwrap();
-    let q_H = zt_V.divide_by_vanishing_poly(H).unwrap();
+    // Round 4
+    let (q_V, q_H) = compute_round_4_polys(
+        &f,
+        &t,
+        &c,
+        &f_hab,
+        &t_hab,
+        &s_f,
+        &s_t,
+        &s_f_rot,
+        &s_t_rot,
+        sum,
+        alpha,
+        zeta,
+        V.clone(),
+        H.clone(),
+    );
+    let z = Fr::from(2);
 }
